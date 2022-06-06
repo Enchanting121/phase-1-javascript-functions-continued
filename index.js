73789749524c524be2a862dae6425d1d74a99c1f
Enchanting121 <samantha.barros95@gmail.com>
2022-06-06 15:23:35 -0400

    commit tree a2d9ebc0f02ab356f25fc628810cc98e7cb51f62

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork = function(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+}
+mondayWork();
+mondayWork("I will work from home");
+
+function wrapAdjective(type = "*") {
+    return function(unique = "special") {
+        return (`You are ${type}${unique}${type}!`);
+    };
+}
+wrapAdjective('||');
